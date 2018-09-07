f5ec81697ac4aaf4fa830b71e1daa6697a800b47
曾長歡 <zengchanghuan@gmail.com>
2018-09-07 09:53:35 +0800

    add file

---
+++ OpenGLDemo/OpenGLDemo/OpenGLView.h
@@ -0,0 +1,13 @@
+//
+//  OpenGLView.h
+//  OpenGLDemo
+//
+//  Created by Zeng Changhuan on 2018/9/6.
+//  Copyright © 2018年 ZengChanghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenGLView : UIView
+
+@end
--- OpenGLDemo/OpenGLDemo/ViewController.m
+++ OpenGLDemo/OpenGLDemo/ViewController.m
@@ -7,23 +7,20 @@
 //
 
 #import "ViewController.h"
-
+#import "OpenGLView.h"
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view = [[OpenGLView alloc] initWithFrame:self.view.bounds];
+
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
